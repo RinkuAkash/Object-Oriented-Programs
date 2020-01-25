d292317fc2604d68225acfc147a722d1c65c47fb
RinkuAkash <akashrinku2@outlook.com>
2020-01-25 16:53:26 +0530

    Added: program to manage inventory data using json

---
+++ 02_Inventory_Data_Management.py
@@ -0,0 +1,87 @@
+import json
+
+"""
+Created on 25/01/2020
+@auther: B Akash
+"""
+"""
+Problem Statement:
+    Create a JSON file having Inventory Details for Rice, Pulses and Wheats 
+        with properties name, weight, price per kg. 
+"""
+
+jsonData = {
+    "Rice": [
+        {"name": "Basmati", "weight": 1000, "price": 95},
+        {"name": "Kurnool", "weight": 840, "price": 44},
+    ],
+    "Pulses": [
+        {"name": "channa", "weight": 900, "price": 112},
+        {"name": "moong", "weight": 850, "price": 129},
+    ],
+    "Wheat": [
+        {"name": "indian", "weight": 830, "price": 55},
+        {"name": "golden", "weight": 790, "price": 40},
+    ],
+}
+
+
+class Inventory:
+    def __init__(self, key):
+        if key == 1:
+            self.item = "Rice"
+        elif key == 2:
+            self.item = "Pulses"
+        elif key == 3:
+            self.item = "Wheat"
+        self.inventory = json.dumps(jsonData, indent=4)
+
+    def getList(self):
+        itemType = self.item
+        unload = json.loads(self.inventory)
+        return unload[itemType]
+
+
+print("Inventory Data Management")
+print("Enter\n1 for Rice\n2 for Pulses\n3 for Wheats")
+
+key = int(input())
+
+if key in [1, 2, 3]:
+    obj = Inventory(key)
+    items = obj.getList()
+
+    for item_index in range(0, len(items)):
+        print(
+            "ID :",
+            item_index,
+            ", Type :",
+            items[item_index]["name"],
+            ", Stock :",
+            items[item_index]["weight"],
+            ", Price :",
+            items[item_index]["price"],
+        )
+
+    ID = int(input("Enter item ID: "))
+    quantity = int(input("Enter quantity(in kgs) : "))
+
+    try:
+        if quantity <= items[ID]["weight"]:
+            bill = json.dumps(
+                {
+                    "item_name": items[ID]["name"],
+                    "Quantity": quantity,
+                    "Cost": quantity * items[ID]["price"],
+                },
+                indent=4,
+            )
+            print("your bill :", bill)
+        else:
+            print("sorry! stock is less than required")
+
+    except IndexError:
+        print("Invalid Id")
+
+else:
+    print("Invalid input")
