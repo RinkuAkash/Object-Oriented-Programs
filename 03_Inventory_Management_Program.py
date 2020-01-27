ef40cb562e80e65a4e8f7346cc0088d3a91a04f0
RinkuAkash <akashrinku2@outlook.com>
2020-01-27 13:22:47 +0530

    Refactored: updated code in PEP format

--- 03_Inventory_Management_Program.py
+++ 03_Inventory_Management_Program.py
@@ -18,54 +18,65 @@ Create InventoryManager to manage the Inventory.
 
 class InventoryFactory:
     def __init__(self):
-        self.item=dict(Rice=[],Pulses=[],Wheat=[])
-    
-    def addValues(self,itemType,name,weight,price_per_kg):
-        self.item[itemType].append({'name':name,'weight':weight,'price_per_kg':price_per_kg})
-    
-    def addTotalPrice(self,itemType,subItem,totalPrice):
-        self.item[itemType][subItem]['total_price']=totalPrice
-    
+        self.item = dict(Rice=[], Pulses=[], Wheat=[])
+
+    def addValues(self, itemType, name, weight, price_per_kg):
+        self.item[itemType].append(
+            {"name": name, "weight": weight, "price_per_kg": price_per_kg}
+        )
+
+    def addTotalPrice(self, itemType, subItem, totalPrice):
+        self.item[itemType][subItem]["total_price"] = totalPrice
+
     def getDetails(self):
-        return json.dumps(self.item,indent=4)
+        return json.dumps(self.item, indent=4)
 
 
 class InventoryManager:
     def __init__(self):
-        self.inventoryObject=InventoryFactory()
-    
+        self.inventoryObject = InventoryFactory()
+
     def addStock(self):
-        for _ in range(int(input("Enter the number of items you want to add to rice : "))):
-            name=input("name : ")
-            weight=int(input("weight : "))
-            price=int(input("price : "))
-            self.inventoryObject.addValues('Rice',name,weight,price)
-        
-        for _ in range(int(input("Enter the number of items you want to add to Pulses : "))):
-            name=input("name : ")
-            weight=int(input("weight : "))
-            price=int(input("price : "))
-            self.inventoryObject.addValues('Pulses',name,weight,price)
+        for _ in range(
+            int(input("Enter the number of items you want to add to rice : "))
+        ):
+            name = input("name : ")
+            weight = int(input("weight : "))
+            price = int(input("price : "))
+            self.inventoryObject.addValues("Rice", name, weight, price)
 
-        for _ in range(int(input("Enter the number of items you want to add to Wheat : "))):
-            name=input("name : ")
-            weight=int(input("weight : "))
-            price=int(input("price : "))
-            self.inventoryObject.addValues('Wheat',name,weight,price)
+        for _ in range(
+            int(input("Enter the number of items you want to add to Pulses : "))
+        ):
+            name = input("name : ")
+            weight = int(input("weight : "))
+            price = int(input("price : "))
+            self.inventoryObject.addValues("Pulses", name, weight, price)
 
+        for _ in range(
+            int(input("Enter the number of items you want to add to Wheat : "))
+        ):
+            name = input("name : ")
+            weight = int(input("weight : "))
+            price = int(input("price : "))
+            self.inventoryObject.addValues("Wheat", name, weight, price)
 
     def calculatePrice(self):
-        stock=json.loads(self.inventoryObject.getDetails())
+        stock = json.loads(self.inventoryObject.getDetails())
+
         for itemType in stock:
-            for subItem in range(0,len(stock[itemType])):
-                total_price=stock[itemType][subItem]['weight'] * stock[itemType][subItem]['price_per_kg']
-                self.inventoryObject.addTotalPrice(itemType,subItem,total_price)
+            for subItem in range(0, len(stock[itemType])):
+                total_price = (
+                    stock[itemType][subItem]["weight"]
+                    * stock[itemType][subItem]["price_per_kg"]
+                )
+                self.inventoryObject.addTotalPrice(itemType, subItem, total_price)
         return self.inventoryObject.getDetails()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Inventory Management Program")
-    manager=InventoryManager()
+    manager = InventoryManager()
     manager.addStock()
     print("Final inventory :")
     print(manager.calculatePrice())
