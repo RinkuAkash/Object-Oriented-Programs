22a0ef40d4a77de8e340e87bebf8c8cde1830a7b
RinkuAkash <akashrinku2@outlook.com>
2020-01-28 15:19:44 +0530

    Refactored: code styled in the form of PEP principle

--- Commercial_Data_Processing/Companies.py
+++ Commercial_Data_Processing/Companies.py
@@ -1,35 +1,36 @@
 import json
 
+
 class Companies:
     def __init__(self):
-        self.file=open("CompanyShares.json")
-        self.List=json.load(self.file)
-    
+        self.file = open("CompanyShares.json")
+        self.List = json.load(self.file)
+
     def printList(self):
-        for info in self.List['StockDetails']:
+        for info in self.List["StockDetails"]:
             print(info)
-    
-    def calculate_price(self,symbol,shares):
-        for info in self.List['StockDetails']:
-            if info['StockSymbol'] == symbol and info['NoOfShare'] <= shares:
+
+    def calculate_price(self, symbol, shares):
+        for info in self.List["StockDetails"]:
+            if info["StockSymbol"] == symbol and info["NoOfShare"] <= shares:
                 return False
-            if info['StockSymbol'] == symbol:
-                info['NoOfShare']-=shares
-                return info['SharePrice']*shares
+            if info["StockSymbol"] == symbol:
+                info["NoOfShare"] -= shares
+                return info["SharePrice"] * shares
         return False
-    
+
     def get_price(self, symbol):
-        for info in self.List['StockDetails']:
-            if info['StockSymbol'] == symbol:
-                return info['SharePrice']
+        for info in self.List["StockDetails"]:
+            if info["StockSymbol"] == symbol:
+                return info["SharePrice"]
         return 0
-    
+
     def add_shares(self, symbol, shares):
-        for info in self.List['StockDetails']:
-            if info['StockSymbol'] == symbol:
-                info['NoOfShare']+=shares
+        for info in self.List["StockDetails"]:
+            if info["StockSymbol"] == symbol:
+                info["NoOfShare"] += shares
                 break
-    
+
     def save(self):
-        with open("CompanyShares.json",'w') as json_file:
-            json.dump(self.List,json_file, indent=4)
+        with open("CompanyShares.json", "w") as json_file:
+            json.dump(self.List, json_file, indent=4)
--- Commercial_Data_Processing/main.py
+++ Commercial_Data_Processing/main.py
@@ -2,33 +2,36 @@ from StockAccount import *
 from Companies import *
 import json
 
-if __name__=='__main__':
+if __name__ == "__main__":
     print("Commercial Data Processing")
     while True:
         print("Enter 1 to create account\n 2 to access account")
         option = int(input())
         if option == 1:
-            userName= input("Enter your name : ")
-            accountFile=open(userName+".json","w+")
-            account={'userDetails':[{
-                'name':userName,
-                'shares':0,
-                'total_value':0,
-                'transactions':[]
-                }],
-                'Stocks':[]
-                }
-            json.dump(account,accountFile)
+            userName = input("Enter your name : ")
+            accountFile = open(userName + ".json", "w+")
+            account = {
+                "userDetails": [
+                    {
+                        "name": userName,
+                        "shares": 0,
+                        "total_value": 0,
+                        "transactions": [],
+                    }
+                ],
+                "Stocks": [],
+            }
+            json.dump(account, accountFile)
             print("Account created successfully")
-        
+
         elif option == 2:
-            filename=input("Enter filename (with extension) : ")
+            filename = input("Enter filename (with extension) : ")
             try:
-                accountFile=open(filename)
+                accountFile = open(filename)
                 break
             except FileNotFoundError:
                 print("Account not available")
-    
+
     account = StockAccount(accountFile)
     companies = Companies()
     while True:
@@ -40,41 +43,40 @@ if __name__=='__main__':
         print("5 print report")
         print("0 to exit")
 
-        option =int(input())
+        option = int(input())
 
         if option == 1:
             print(account.valueOf())
-        
+
         elif option == 2:
             companies.printList()
-            symbol=input("Enter symbol : ")
-            shares=int(input("Enter no of shares : "))
-            check=companies.calculate_price(symbol,shares)
+            symbol = input("Enter symbol : ")
+            shares = int(input("Enter no of shares : "))
+            check = companies.calculate_price(symbol, shares)
             if check is False:
                 print("Transaction unsuccessful")
             else:
-                amount=check
+                amount = check
                 account.buy(amount, shares, symbol)
                 print("Transaction successfull")
-        
+
         elif option == 3:
-            symbol=input("Enter symbol : ")
-            shares=int(input("Enter no. of shares : "))
+            symbol = input("Enter symbol : ")
+            shares = int(input("Enter no. of shares : "))
             share_price = companies.get_price(symbol)
-            if share_price>-1 and account.sell(shares, symbol, share_price):
+            if share_price > -1 and account.sell(shares, symbol, share_price):
                 companies.add_shares(symbol, shares)
                 print("Transaction successful")
             else:
                 print("Transaction unsuccessful")
 
-        
         elif option == 4:
             account.save(filename)
             companies.save()
-        
+
         elif option == 5:
             account.printReport()
-        
+
         elif option == 0:
             break
 
