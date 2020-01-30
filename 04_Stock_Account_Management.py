503e0c5b9613936a5d33c235887dacb874b70cda
RinkuAkash <akashrinku2@outlook.com>
2020-01-30 14:53:20 +0530

    Refactored: updated code style as per PEP principles

--- 04_Stock_Account_Management.py
+++ 04_Stock_Account_Management.py
@@ -4,8 +4,8 @@ Created on 25/01/2020
 """
 """
 Problem statement:
-Write a program to read in Stock Names, Number of Share, Share Price. 
-Print a Stock Report with total value of each Stock and the total value of Stock.
+Write a program to read in Stock Names, Number of Share, Share Price. Print
+ a Stock Report with total value of each Stock and the total value of Stock.
 """
 
 
@@ -13,7 +13,7 @@ class Stock:
     def __init__(self):
         self.stocks = []
 
-    def addValues(self, name_of_share, no_of_shares, price_of_share):
+    def add_values(self, name_of_share, no_of_shares, price_of_share):
         self.stocks.append([name_of_share, no_of_shares, price_of_share])
 
 
@@ -21,9 +21,11 @@ class Stock_portfolio(Stock):
     def __init__(self):
         super().__init__()
 
-    def printPortfolio(self):
+    def print_portfolio(self):
         for stock in self.stocks:
-            print(stock[0], "|", stock[1], "|", stock[2], "|", stock[2] * stock[1])
+            print(
+                stock[0], "|", stock[1], "|", stock[2], "|", stock[2] * stock[1]
+            )
 
 
 if __name__ == "__main__":
@@ -35,7 +37,7 @@ if __name__ == "__main__":
         name_of_share = input("Enter share name: ")
         no_of_shares = int(input("Enter no. of shares: "))
         price_of_share = int(input("Enter price of each share: "))
-        stock.addValues(name_of_share, no_of_shares, price_of_share)
+        stock.add_values(name_of_share, no_of_shares, price_of_share)
 
     print(
         "Name of the share",
@@ -46,4 +48,4 @@ if __name__ == "__main__":
         "|",
         "Total Value",
     )
-    stock.printPortfolio()
+    stock.print_portfolio()
