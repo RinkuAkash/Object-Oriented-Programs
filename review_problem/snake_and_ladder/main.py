9ed0268250e427d8274d1336d9b8b4363ac6372b
RinkuAkash <akashrinku2@outlook.com>
2020-01-31 19:15:11 +0530

    Added: completed review problem by adding two players to snake and ladder game

--- review_problem/snake_and_ladder/main.py
+++ review_problem/snake_and_ladder/main.py
@@ -2,12 +2,17 @@ from snake_and_ladder import SnakeAndLadder
 
 if __name__ == "__main__":
     print("The Snake And Ladder Game")
-    user_name = input("Enter your name to start : ")
-    player1 = SnakeAndLadder(user_name)
-    while player1.play_game():
-        print("After rolling a die,",
-              player1.user_name,
-              "at",
-              player1.position,
-              "position")
+
+    # Taking players name as input
+    player1_name = input('Enter player1 name : ')
+    player2_name = input("Enter player2 name : ")
+    player1 = SnakeAndLadder(player1_name)
+    player2 = SnakeAndLadder(player2_name)
+
+    # Repeating game till any player wins
+    while True:
+        if not player1.play_game:
+            break
+        if not player2.play_game:
+            break
     print("Dice roll count : ", player1.roll_count)
--- review_problem/snake_and_ladder/snake_and_ladder.py
+++ review_problem/snake_and_ladder/snake_and_ladder.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# Static method to roll dice using random function1
 def roll_die():
     return np.random.randint(1, 7)
 
@@ -11,9 +12,24 @@ class SnakeAndLadder:
         self.position = 0
         self.roll_count = 0
 
+    # Method to check position of player with conditions
+    def check_position_count(self, steps):
+        if self.position + steps > 100:
+            print("No moment")
+            return True
+        elif self.position + steps == 100:
+            print("Congratulations,", self.user_name, "won")
+            return False
+        else:
+            self.position += steps
+            return True
+
+    # Method to play game, returns True if game continues
     def play_game(self):
         steps = roll_die()
         self.roll_count += 1
+
+        # option is used to get snake, ladder or no play
         option = np.random.randint(0, 3)
         if option == 0:
             self.position -= steps
@@ -21,20 +37,14 @@ class SnakeAndLadder:
                 self.position = 0
             print("You got snake")
         elif option == 1:
-            if self.position + steps > 100:
-                print("No moment")
-            elif self.position + steps == 100:
-                print("Congratulations, you won")
+            if self.check_position_count(steps):
+                print("you got ladder")
             else:
-                self.position += steps
-                print("You got ladder")
+                return False
         else:
-            if self.position + steps > 100:
-                print("No moment")
-            else:
-                self.position += steps
-                if self.position == 100:
-                    print("Congratulations, you won")
-                    return False
+            if self.check_position_count(steps) is False:
+                return False
+        print("After rolling a die,", self.user_name,
+              "at", self.position, "position")
 
         return True
