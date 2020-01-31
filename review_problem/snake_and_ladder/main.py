7ab0379f6956a06fdccef38ce7351941ba958564
RinkuAkash <akashrinku2@outlook.com>
2020-01-31 17:24:20 +0530

    Added: add roll die to change user position feature

--- review_problem/snake_and_ladder/main.py
+++ review_problem/snake_and_ladder/main.py
@@ -1,7 +1,11 @@
 from snake_and_ladder import SnakeAndLadder
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("The Snake And Ladder Game")
     user_name = input("Enter your name to start : ")
     player1 = SnakeAndLadder(user_name)
-    print(player1.user_name, 'at', player1.position, 'position')
+    print(player1.user_name, "at", player1.position, "position")
+    player1.play_game()
+    print("After rolling a die,", player1.user_name,
+          "at", player1.position, "position"
+          )
--- review_problem/snake_and_ladder/snake_and_ladder.py
+++ review_problem/snake_and_ladder/snake_and_ladder.py
@@ -1,23 +1,15 @@
+import numpy as np
+
+
+def roll_die():
+    return np.random.randint(1,7)
+
+
 class SnakeAndLadder:
     def __init__(self, user_name):
         self.user_name = user_name
         self.position = 0
-        self.SNAKES = {
-            32: 10,
-            36: 6,
-            48: 26,
-            62: 18,
-            88: 24,
-            95: 56,
-            97: 78
-        }
-        self.LADDERS = {
-            1: 38,
-            4: 14,
-            8: 30,
-            21: 42,
-            28: 76,
-            50: 67,
-            71: 92,
-            80: 99
-        }
+
+    def play_game(self):
+        steps = roll_die()
+        self.position += steps
